3c8b355191a2b7cbae2f2ff65b48206bb873fb19
abovespirit <crc91118@gmail.com>
2024-09-22 14:49:29 -0400

    .

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,7 @@ particlesJS("particles-js", {
           "nb_sides": 5
         },
         "image": {
-          "src": "../img/Website-Content/rifle.png",
+          "src": "img/github.svg",
           "width": 100,
           "height": 100
         }
